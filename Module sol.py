2718745b36a3be88f32753fa98493c772c53be04
lajouanie <simon.lajouanie@student.ecp.fr>
2017-03-14 11:07:20 +0100

    Add files via upload

--- Module sol.py
+++ Module sol.py
@@ -2,6 +2,7 @@ from numpy import *
 from math import *
 #Ne pas oublier l'initialisation , module azote dispo azote dispo
 def fmin(n,T) :
+#T la temperature et n le jour
     if T[n]==0 :
         return 0
     else :
@@ -11,7 +12,7 @@ def fmin(n,T) :
             if T[i]>0 :
                 s+=T[i]
         return T[m]/s
-def Tmoy(T) :
+def Tmoy(T,n) :
     m=len(T)
     s=0
     n=0
@@ -21,17 +22,29 @@ def Tmoy(T) :
         n+=1
     return s/n
         
-#fonction temps normalisé
+#fonction temps normalisé 
 def Mresidu(Mr,fmin):
     s=Mr*fmin
     return s
-#residu azoté disponible
+#residu azoté disponible, Mr est le coefficient de mineralisation des residus de culture
 def Mcompoorga(Ma,fmin):
     s=Ma*fmin
     return s
-#composé organique
-def Mhumus(fdsc,a,Tmoy,Norga,Da,T) :
+#composé organique, Ma coefficient de mineralisation des composes organiques
+def Mhumus(fdsc,a,beta,delta,Tmoy,Tref,Norga,Da,ep,Beta,pcarg,CaC03,d) :
+    #pc arg représente le % d'argile
     if T<!0 :
         return 0
     else :
-        Mh=fdsc*?*exp(a*(Tmoy-Tref)*Norga*Da*ep*1000*(Beta/((%arg+?)*(CaCO3+d)))
+        Mh=fdsc*beta*exp(a*(Tmoy-Tref)*Norga*Da*ep*1000*(Beta/((pcarg+delta)*(CaCO3+d))))
+        return Mh
+#fdc le coefficient relatif à la gestion de la parcelle considérée
+#Norganique représente le pourcentage d’azote total de la parcelle
+#Da la densité apparente de l’horizon labouré 
+# ep l’épaisseur de l’horizon labouré 
+# %arg et CaCO3, respectivement les pourcentages d’argile et de calcaire de l’horizon labouré
+
+def CaU(mu,MS,MS7,SDT,SDT1,l) :
+    #l et mu représentent respectivement l’ordonnée à l’origine et la pente de la relation linéaire entre le CAU et la vitesse de croissance de la culture dans les sept jours précédents l’apport [Ln(MSj/10) – ln(MSj-7/10)/(SDTj – SDTj-7)].
+    CaU=min(100,l+(mu*100*(((ln(MS/10) – ln(MS7/10))*(MS/10))/(SDT–SDT7))))
+    return CaU
