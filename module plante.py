b936e6dda07391f428460915b8392417f70d88c8
elenaam3 <elena.matias-casacuberta@student.ecp.fr>
2017-04-03 18:37:00 +0200

    Update module plante.py

--- module plante.py
+++ module plante.py
@@ -1,8 +1,12 @@
 #module matière sèche 
-def Matseche(n,MS,eimax,k,LAI,ebmax,D,QNcrit):
-    ei=effintercept(eimax,k,LAI,D,QNcrit)
-    eb=econvlum(ebmax,k,LAI,D,QNcrit)
+def Matseche(n,MS,eimax,k,ebmax,D,QNcrit):#j'ai enlevé le LAI, on le calcule non ?
+    ei=effintercept(eimax,k,D,QNcrit)
+    eb=econvlum(ebmax,k,D,QNcrit)
     MS+=[MS(n-1)+ei[n]+eb[n]]
     return MS
-def effintercept(eimax,k,LAI,D,QNcrit):
+def effintercept(eimax,k,D,QNcrit): #j'ai enlevé le LAI, on le calcule non ?
+    LAI = D*QNcrit
+    ei = eimax*(1-exp(-k*LAI))
+    return ei
+def econvlum(ebmax,k,D,QNcrit):
     
