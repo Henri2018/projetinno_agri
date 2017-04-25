3fbc27fe4f0c76e0715836f84a4468b4fdda58db
Henri2018 <henri.desesquelles@student.ecp.fr>
2017-04-25 15:23:44 +0200

    Update module plante.py

--- module plante.py
+++ module plante.py
@@ -1,4 +1,5 @@
-from math import exp
+from scipy import *
+from scipy.optimize import brentq
 
 ###############################################################################
 ############################  hypothèse du programme   ########################
@@ -13,19 +14,33 @@ D: fraction interceptée supposé constante
 ##################            code           ##################################
 ###############################################################################
 #initialisation
+ebmax=2.8
+eimax=0.96
+k=0.72
+D=0.028
+R=0 # A trouver
+Vmax=0 # A trouver
+Ncrit=[4.4,5.35,-0.442]
+Nmax=[0,0] # A trouver
+MSseuil=0 # A trouver
+MS=dict()
+QN=dict()
+Tmoy=dict()
+PARi=dict()
+LAI= dict()
+
 
 ei=list()
 eb=list()
 INNI=0
 INN0=0
-
 ## AVANT FLORAISON
 
 #module matière sèche 
-def Matseche(n,MS,PARi,eimax,k,ebmax,LAI):
+def Matseche(n):
     '''j'ai enlevé le LAI, on le calcule non ? NON
     n: jour concerné
-    MS: matière séche dans la plante, dictionnaire contenant les jours av n - clés = entier, valeurs = chiffres
+    MS: matière séche dans la plante, dictionnaire contenant les jours av n
     PARi: rayonnement photosynthètique actif, dictionnaire contenant les n jours
         PARi[n]=D*RG[n]
     RG :  rayonnement global incident, dictionnaire
@@ -42,11 +57,159 @@ def Matseche(n,MS,PARi,eimax,k,ebmax,LAI):
     D: fraction reliant LAI à QNcr
         =0.028 (valeur venant de biblio inconnue)
     QNcrit
+    Nous pouvions étudier 4 cas de figure différents :
+    Sans carence
+        Cas 1 : MSj/1000<MSseuil
+        Cas 2 : sinon
+    Avec Carence
+        Cas 3 : MSj/1000<MSseuil
+        cas 4 : sinon 
     '''
-    effintercept(eimax,k,LAI)
-    econvlum(ebmax,k,LAI)
-    MS[n]=[MS.get(n-1)+ei[n]+eb[n]*PARi[n]]  #ça serait plutôt +eb*PARi et on crée la nouvelle valeur dans MS associee a n
-    return MS.get(n)
+    #HYPOTHESE 0:
+    #On suppose qu'on a pas de carence azotée on boucle pour vérifier
+    INN_0,INN_1=1,1
+    
+    #HYPOTHESE 1:
+    #Obliger de remplacer MS(n) par MS(n-1) donc boucle while pour vérifier à 
+    #posteriori du calcul que l'hypothèse est valable
+    
+    #Initialisation de la boucle de vérification de l'hypothèse 0
+    #initialisation de la boucle de vérification de l'hypothèse 1
+    MS1,MS2=MS.get(n-1),MS.get(n-1)
+    
+    #Sans carence
+    
+    #cas1
+    if (MS2/1000)<MSseuil:
+        f=lambda x: f_cas1(x,MS1,PARi(n))
+        MS1=MS2
+        MS2=brentq(f,MS1,MS1*2)
+    #cas 2
+    else:
+        f=lambda x: f_cas2(x,MS1,PARi(n))
+        MS1=MS2
+        MS2=brentq(f,MS1,MS1*2)
+    #Boucle pour vérifier l'hypothèse 1
+    while ((MS1/1000<MSseuil and MS2/1000>=MSseuil) or (MS1/1000>=Sseuil and MS2/1000<MSseuil)):
+        #cas1
+        if (MS2/1000)<MSseuil:
+            f=lambda x: f_cas1(x,MS1,PARi(n))
+            MS1=MS2
+            MS2=brentq(f,MS1,MS1*2)
+        #cas 2
+        else:
+            f=lambda x: f_cas2(x,MS1,PARi(n))
+            MS1=MS2
+            MS2=brentq(f,MS1,MS1*2)
+            
+    # Vérification de l'hypothèse 0 après l'initialisation
+    delta_MS=MS2-MS.get(n-1)
+    c1=Vmax*Tmoy.get(n)
+    Mn_n=Mn.get(n)
+    QN_n_1=QN.get(n-1)
+    if (MS2/1000)<MSseuil:
+        N_n=min(Mn_n,min(R*Nmax[0]*(delta_MS),c1))
+        QN[n]=QN_n_1+N_n
+        INN_1=N_n/(delta_MS*Ncrit[0])
+    else:
+        N_n=min(Mn_n,min(R*Nmax[1]*(delta_MS),c1))
+        QN[n]=QN_n_1+N_n
+        INN_1=N_n/(delta_MS*Ncrit[1]*(MS2/1000)**Ncrit[2])
+    
+    # Boucle de vérification de l'hypothèse 0
+    while ((INN_0<1 and INN_1>=1) or (INN_0>=1 and INN_0<1)):
+        if (INN_1<1):
+            #Dernière boucle donne carence alors qu'avant non
+            #cas3
+            if (MS2/1000)<MSseuil:
+                f=lambda x: f_cas3(x,MS1,PARi(n))
+                MS1=MS2
+                MS2=brentq(f,MS1,MS1*2)
+            #cas 4
+            else:
+                f=lambda x: f_cas4(x,MS1,PARi(n))
+                MS1=MS2
+                MS2=brentq(f,MS1,MS1*2)
+            while (MS1/1000<MSseuil and MS2/1000>=MSseuil) or (MS1/1000>=Sseuil and MS2/1000<MSseuil):
+                #cas3
+                if (MS2/1000)<MSseuil:
+                    f=lambda x: f_cas3(x,MS1,PARi(n))
+                    MS1=MS2
+                    MS2=brentq(f,MS1,MS1*2)
+                #cas 4
+                else:
+                    f=lambda x: f_cas4(x,MS1,PARi(n))
+                    MS1=MS2
+                    MS2=brentq(f,MS1,MS1*2)
+        else:
+            #Dernière boucle donne sans carence
+            #cas1
+            if (MS2/1000)<MSseuil:
+                f=lambda x: f_cas1(x,MS1,PARi(n))
+                MS1=MS2
+                MS2=brentq(f,MS1,MS1*2)
+            #cas 2
+            else:
+                f=lambda x: f_cas2(x,MS1,PARi(n))
+                MS1=MS2
+                MS2=brentq(f,MS1,MS1*2)
+            while (MS1/1000<MSseuil and MS2/1000>=MSseuil) or (MS1/1000>=Sseuil and MS2/1000<MSseuil):
+                #cas1
+                if (MS2/1000)<MSseuil:
+                    f=lambda x: f_cas1(x,MS1,PARi(n))
+                    MS1=MS2
+                    MS2=brentq(f,MS1,MS1*2)
+                #cas 2
+                else:
+                    f=lambda x: f_cas2(x,MS1,PARi(n))
+                    MS1=MS2
+                    MS2=brentq(f,MS1,MS1*2)
+        delta_MS=MS2-MS.get(n-1)
+        INN_0=INN_1
+        if (MS2/1000)<MSseuil:
+            N_n=min(Mn_n,min(R*Nmax[0]*(delta_MS),c1))
+            QN[n]=QN_n_1+N_n
+            INN_1=N_n/(delta_MS*Ncrit[0])
+        else:
+            N_n=min(Mn_n,min(R*Nmax[1]*(delta_MS),c1))
+            QN[n]=QN_n_1+N_n
+            INN_1=N_n/(delta_MS*Ncrit[1]*(MS2/1000)**Ncrit[2])
+    #Avec Carence
+       
+
+    MS[n]=MS2
+    return MS
+    
+def f_cas1(x,x1,PAR):
+    y0=x-x1
+    y1=1-exp(-k*D*Ncrit[0]*y0)
+    y2=ebmax*eimax*PAR
+    return y0-y1*y2
+
+def f_cas2(x,x1,PAR):
+    y=Ncrit[1]*(x/1000)**Ncrit[2]
+    y0=x-x1
+    y1=1-exp(-k*D*y*y0)
+    y2=ebmax*eimax*PAR
+    return y0-y1*y2
+    
+def f_cas3(x,x1,PAR):
+    y0=x-x1
+    y1=1-exp(-k*D*Ncrit[0]*y0)
+    y2=ebmax*eimax*PAR
+    return y0-y1*y2
+
+def f_cas4(x,x1,PAR):
+    eb=calcul_eb_carence()
+    y=Ncrit[1]*x**Ncrit[2]
+    y0=x-x1
+    y1=1-exp(-k*D*y*y0)
+    y2=ebmax*eimax*PAR
+    return y0-y1*y2    
+
+
+def calcul_eb_carence():
+    Nperc=
 def effintercept(eimax,k,LAI): 
     '''j'ai enlevé le LAI, on le calcule non ? NON LAI = D*QNcrit
     '''
